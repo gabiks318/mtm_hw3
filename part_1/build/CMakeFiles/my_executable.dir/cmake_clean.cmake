a5db519171ec5d99dcc468db4f12d7c80c6662a0
Gabriel Kositsky <gabiks318@gmail.com>
2021-01-04 11:33:49 +0200

    configured project, added files

---
+++ part_1/build/CMakeFiles/my_executable.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_executable.dir/main.cpp.obj"
+  "libmy_executable.dll.a"
+  "my_executable.exe"
+  "my_executable.exe.manifest"
+  "my_executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
